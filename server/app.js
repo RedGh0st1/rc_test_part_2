a3df64937af222fbf36fca6cac131e2012fca97e
Lennie Nurse <lnursej@gmail.com>
2024-10-21 08:21:16 -0400

    made .env and add the secretkey to file , rate limiter to limit , validation for wrong login to username or password, added token expiration time, added .env to gitignore file

--- server/app.js
+++ server/app.js
@@ -2,24 +2,53 @@ const express = require("express");
 const jwt = require("jsonwebtoken");
 const logger = require("morgan");
 const cors = require("cors");
+const rateLimit = require("express-rate-limit");
+require("dotenv").config();
 
 const app = express();
-const secretKey = "supersecretkey";
+const secretKey = process.env.JWT_SECRET;
 
+// rate limiter entries
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per
+  standardHeaders: true, //Enable standard rate limit headers
+  message: "Too many requests, please try again later",
+});
+
+// implement cors(CSRF) protection
 app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
+app.use(limiter);
 
-const users = [{ id: 1, username: "admin", password: "password" }];
+const users = [
+  {
+    id: 1,
+    username: "admin",
+    password: "password",
+  },
+];
 
-app.post("/login", (req, res) => {
+app.post("/login", async (req, res) => {
   const { username, password } = req.body;
+
+  // looking for the user
   const user = users.find(
     (u) => u.username === username && u.password === password
   );
+  //
+  if (!user || user.password !== password) {
+    return res.status(401).json({ message: "Invalid credentials" });
+  }
 
   if (user) {
-    const token = jwt.sign({ id: user.id, username: user.username }, secretKey);
+    const token = jwt.sign(
+      { id: user.id, username: user.username },
+      secretKey,
+      { expiresIn: "1h" }
+    ); //add token expiration
+
     res.json({ token });
   } else {
     res.status(401).json({ message: "Invalid credentials" });
